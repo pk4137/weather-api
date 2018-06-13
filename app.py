6552ac91a3104137cc8bdc85edde17ec1c314332
pk4137 <40210301+pk4137@users.noreply.github.com>
2018-06-13 09:11:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
